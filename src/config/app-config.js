b1c480fc636c98e90b38291c4c94d28a21fee56c
jgarciatorralba <jgarciatorralba@gmail.com>
2020-11-05 19:41:51 +0100

    Add 'db.cluster', remove 'db.host' and 'db.port'

--- src/config/app-config.js
+++ src/config/app-config.js
@@ -22,8 +22,7 @@ export default {
     clientDomain: process.env.CLIENT_DOMAIN,
   },
   db: {
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT),
+    cluster: process.env.DB_CLUSTER,
     user: process.env.DB_USER,
     pass: process.env.DB_PASS,
     name: process.env.DB_NAME,
