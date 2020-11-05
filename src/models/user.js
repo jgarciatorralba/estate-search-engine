e7f85191d1325fdf3c65817b6450b7c759084868
jgarciatorralba <jgarciatorralba@gmail.com>
2020-11-05 19:42:49 +0100

    Add file 'User.js' with user model on mongoose

--- src/models/user.js
+++ src/models/user.js
@@ -1,1 +1,26 @@
-// User model with mongoose
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  avatar: {
+    type: String,
+    default: null,
+  },
+});
+
+export const User = mongoose.model("User", UserSchema);
