dacf19e22f8cdee46ac85283c33be9e8a84bda49
jgarciatorralba <jgarciatorralba@gmail.com>
2020-11-07 17:38:03 +0100

    Add timestamps to mongoose models

--- src/models/Client.js
+++ src/models/Client.js
@@ -2,31 +2,32 @@ import mongoose from "mongoose";
 import validator from "validator";
 import mongoose_delete from "mongoose-delete";
 
-const ClientSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
+const ClientSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: [validator.isEmail, "Invalid email format"],
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    avatar: {
+      type: String,
+      default: "default.jpg",
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    validate: [validator.isEmail, "Invalid email format"],
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  date: {
-    type: Date,
-    default: Date.now,
-  },
-  avatar: {
-    type: String,
-    default: "default.jpg",
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 ClientSchema.plugin(mongoose_delete, {
   deletedAt: true,
--- src/models/Purchase.js
+++ src/models/Purchase.js
@@ -1,100 +1,105 @@
 import mongoose from "mongoose";
 
-const PurchaseSchema = new mongoose.Schema({
-  buyer_id: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  price: {
-    type: Number,
-    required: true,
-  },
-  currency: {
-    type: String,
-    enum: ["EUR", "USD"],
-    default: "EUR",
-  },
-  propertyType: {
-    type: String,
-    enum: ["Home", "Office"],
-    required: true,
-  },
-  homeType: {
-    type: String,
-    enum: ["Flat/Apartment", "House", "Duplex", "Penthouse"],
-    required: function () {
-      return this.propertyType == "Home";
+const PurchaseSchema = new mongoose.Schema(
+  {
+    buyer_id: {
+      type: String,
+      required: true,
     },
-  },
-  numBedrooms: {
-    type: String,
-    enum: ["0", "1", "2", "3", "4 or +"],
-    required: function () {
-      return this.propertyType == "Home";
+    description: {
+      type: String,
+      required: true,
+      trim: true,
     },
-  },
-  numBathrooms: {
-    type: String,
-    enum: ["1", "2", "3 or +"],
-    required: function () {
-      return this.propertyType == "Home";
-    },
-  },
-  equipment: {
-    type: String,
-    enum: ["Indifferent", "Fully fitted kitchen", "Furnished"],
-    required: function () {
-      return this.propertyType == "Home";
-    },
-  },
-  condition: {
-    type: String,
-    enum: ["New", "Good condition", "Needs renovation"],
-    required: function () {
-      return this.propertyType == "Home";
-    },
-  },
-  buildingUse: {
-    type: String,
-    enum: ["Private", "Co-working", "Security System"],
-    required: function () {
-      return this.propertyType == "Office";
-    },
-  },
-  publicationDate: {
-    type: Date,
-    default: Date.now,
-  },
-  location: {
-    latitude: {
+    price: {
       type: Number,
       required: true,
     },
-    longitude: {
-      type: Number,
+    currency: {
+      type: String,
+      enum: ["EUR", "USD"],
+      default: "EUR",
+    },
+    propertyType: {
+      type: String,
+      enum: ["Home", "Office"],
       required: true,
     },
+    homeType: {
+      type: String,
+      enum: ["Flat/Apartment", "House", "Duplex", "Penthouse"],
+      required: function () {
+        return this.propertyType == "Home";
+      },
+    },
+    numBedrooms: {
+      type: String,
+      enum: ["0", "1", "2", "3", "4 or +"],
+      required: function () {
+        return this.propertyType == "Home";
+      },
+    },
+    numBathrooms: {
+      type: String,
+      enum: ["1", "2", "3 or +"],
+      required: function () {
+        return this.propertyType == "Home";
+      },
+    },
+    equipment: {
+      type: String,
+      enum: ["Indifferent", "Fully fitted kitchen", "Furnished"],
+      required: function () {
+        return this.propertyType == "Home";
+      },
+    },
+    condition: {
+      type: String,
+      enum: ["New", "Good condition", "Needs renovation"],
+      required: function () {
+        return this.propertyType == "Home";
+      },
+    },
+    buildingUse: {
+      type: String,
+      enum: ["Private", "Co-working", "Security System"],
+      required: function () {
+        return this.propertyType == "Office";
+      },
+    },
+    publicationDate: {
+      type: Date,
+      required: true,
+    },
+    location: {
+      latitude: {
+        type: Number,
+        required: true,
+      },
+      longitude: {
+        type: Number,
+        required: true,
+      },
+    },
+    images: {
+      type: [String],
+    },
+    services: {
+      type: [String],
+      enum: [
+        "Pets allowed",
+        "Lift",
+        "Air conditioning",
+        "Garden",
+        "Swimming pool",
+        "Terrace",
+      ],
+    },
   },
-  images: {
-    type: [String],
-  },
-  services: {
-    type: [String],
-    enum: [
-      "Pets allowed",
-      "Lift",
-      "Air conditioning",
-      "Garden",
-      "Swimming pool",
-      "Terrace",
-    ],
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Purchase = mongoose.model("Purchase", PurchaseSchema);
 
