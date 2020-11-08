aa9589cf8028c0f2cf25740ec010b0d0026d8303
jgarciatorralba <jgarciatorralba@gmail.com>
2020-11-08 18:26:32 +0100

    Add routes to get properties (forward request to laravel server)

--- src/routers/property.js
+++ src/routers/property.js
@@ -1,8 +1,9 @@
 // Import native node modules
-import path from "path";
+import querystring from "querystring";
 
 // Import dependencies
 import express from "express";
+import axios from "axios";
 
 // Import project files
 import config from "../config/app-config.js";
@@ -12,13 +13,70 @@ const router = express.Router();
 
 router.use(authMiddleware);
 
-//TODO: requests from front-end to be forwarded to back-end
-
 // Get all existing properties
+router.get("/", async (req, res) => {
+  try {
+    const properties = await axios.get(
+      config.app.laravelServerDomain + "/product",
+      {
+        headers: {
+          Authorization: "Bearer " + config.app.apiSecret,
+        },
+      }
+    );
+    res.json({ data: properties, error: null });
+  } catch (error) {
+    return res.status(400).json({ data: null, error: "Error: " + error });
+  }
 
-// Get details for a given property
+  // Testing only
+  // res.json({ data: "test route '/property'", error: null });
+});
 
 // Get newly created properties (last 24h)
+router.get("/new", async (req, res) => {
+  try {
+    const properties = await axios.get(
+      config.app.laravelServerDomain + "/product/new",
+      {
+        headers: {
+          Authorization: "Bearer " + config.app.apiSecret,
+        },
+      }
+    );
+    res.json({ data: properties, error: null });
+  } catch (error) {
+    return res.status(400).json({ data: null, error: "Error: " + error });
+  }
+
+  // Testing only
+  // res.json({ data: "test route '/property/new'", error: null });
+});
+
+// Get details for a given property
+router.get("/detail/:property_id", async (req, res) => {
+  try {
+    const property = await axios.get(
+      config.app.laravelServerDomain +
+        "/product/detail/" +
+        req.params.property_id,
+      {
+        headers: {
+          Authorization: "Bearer " + config.app.apiSecret,
+        },
+      }
+    );
+    res.json({ data: property, error: null });
+  } catch (error) {
+    return res.status(400).json({ data: null, error: "Error: " + error });
+  }
+
+  // Testing only
+  // res.json({
+  //   data: "test route '/property/detail/" + req.params.property_id + "'",
+  //   error: null,
+  // });
+});
 
 /** Get properties with filters:
  * Type of property: "Home" or "Office"
@@ -33,5 +91,30 @@ router.use(authMiddleware);
  * publicationDate: last 24h, last week, last month
  * Services: "Pets allowed", "Lift", "Air conditioning", "Garden", "Swimming pool", "Terrace"
  */
+router.get("/filter", async (req, res) => {
+  const url = new URL(
+    "http://" + config.app.nodeServerDomain + req.originalUrl
+  );
+  let searchStr = url.search;
+
+  try {
+    const properties = await axios.get(
+      config.app.laravelServerDomain + "/product/filter" + searchStr,
+      {
+        headers: {
+          Authorization: "Bearer " + config.app.apiSecret,
+        },
+      }
+    );
+    res.json({ data: properties, error: null });
+  } catch (error) {
+    return res.status(400).json({ data: null, error: "Error: " + error });
+  }
+
+  // Testing only
+  // searchStr = searchStr.substring(1);
+  // const queryParams = querystring.parse(searchStr);
+  // res.json({ data: queryParams, error: null });
+});
 
 export default router;
