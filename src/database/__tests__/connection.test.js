0d33554d1c11192e5ddb846cc889c3892bdab1e4
jgarciatorralba <jgarciatorralba@gmail.com>
2020-11-09 17:49:17 +0100

    Add first test file 'connection.test.js'

---
+++ src/database/__tests__/connection.test.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+import Database from "../connection.js";
+
+describe("Test the database connection", () => {
+  beforeAll(async () => {
+    await mongoose.connection.close();
+  });
+
+  test("Should connect to the database", (done) => {
+    const db = new Database();
+    const connPromise = db.connect();
+    connPromise.then((result) => {
+      expect(result).toBe("MongoDB connected...");
+      done();
+    });
+  });
+
+  test("Should disconnect from the database", (done) => {
+    const db = new Database();
+    const connPromise = db.close();
+    connPromise.then((result) => {
+      expect(result).toBe("MongoDB connection closed...");
+      done();
+    });
+  });
+});
