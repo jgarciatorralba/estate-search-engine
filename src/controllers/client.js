18cb5967485a2d7d5459f34f23122eb41be65775
jgarciatorralba <jgarciatorralba@gmail.com>
2020-11-10 11:34:00 +0100

    Add client controller separate implementation from router

---
+++ src/controllers/client.js
@@ -0,0 +1,60 @@
+// Import dependencies
+import bcrypt from "bcrypt";
+
+// Import models
+import Client from "../models/Client.js";
+
+// Import config object
+import config from "../config/app-config.js";
+
+export default {
+  create: async function (clientObj) {
+    const newClient = new Client({
+      email: clientObj.email,
+      password: await bcrypt.hash(clientObj.password, config.app.saltRounds),
+      username: clientObj.username,
+    });
+
+    try {
+      await newClient.save();
+    } catch (error) {
+      return error;
+    }
+
+    return null;
+  },
+
+  findByEmail: async function (email) {
+    const client = await Client.findOne({ email: email });
+    return client;
+  },
+
+  findById: async function (id) {
+    const client = await Client.findOne({ _id: id }, [
+      "_id",
+      "email",
+      "username",
+      "avatar",
+    ]);
+    return client;
+  },
+
+  findDeletedById: async function (id) {
+    const deletedClient = await Client.findOneDeleted({ _id: id });
+    return deletedClient;
+  },
+
+  updateClientById: async function (id, clientObj) {
+    try {
+      await Client.updateOne({ _id: id }, clientObj);
+    } catch (error) {
+      return error;
+    }
+
+    return null;
+  },
+
+  deleteClient: async function (clientObj) {
+    await clientObj.delete();
+  },
+};
